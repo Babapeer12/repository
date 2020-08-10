27ceb64de391703fd8c324c079bec46ec6157907
BABAPEER <BABAPEER@LAPTOP-9SNAFCN3>
2020-08-10 19:34:30 +0530

    commit

--- Actitime/src/test/java/com/actitime/timetrack/Demo2.java
+++ Actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -1,5 +1,15 @@
 package com.actitime.timetrack;
 
-public class Demo2 {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2
+{
+	@Test
+	public void runTest()
+	{
+		Reporter.log("Hi can you see me in Jenkins");
+		
+	}
 
 }
